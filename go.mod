ead9f1980593a08d814b4534d484463ad5e1dc72
Khanh Phan <khanhphan@beeketing.net>
2024-04-04 23:47:14 +0700

    add result

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.7
 
 require (
 	github.com/aquasecurity/trivy v0.50.1
+	github.com/olekukonko/tablewriter v0.0.5
 	github.com/segmentio/kafka-go v0.4.47
 )
 
@@ -12,8 +13,10 @@ require (
 	github.com/google/go-containerregistry v0.19.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/package-url/packageurl-go v0.1.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/samber/lo v1.39.0 // indirect
 	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/aquasecurity/trivy/pkg/types"
+	"github.com/olekukonko/tablewriter"
 	"github.com/segmentio/kafka-go"
 )
 
@@ -20,53 +21,111 @@ func main() {
 }
 
 func run() error {
-	kafkaTopic := flag.String("topic", "", "Topic kafka")
-	if kafkaTopic == nil || *kafkaTopic == "" {
+	topic := flag.String("topic", "", "Kafka topic")
+	brokers := flag.String("brokers", "", "Kafka brokers, comma separated")
+	flag.Parse()
+
+	if *topic == "" {
 		return fmt.Errorf("topic is required")
 	}
 
-	kafkaBrokers := flag.String("brokers", "", "Brokers kafka")
-	if kafkaBrokers == nil || *kafkaBrokers == "" {
-		return fmt.Errorf("cluster is required")
+	if *brokers == "" {
+		return fmt.Errorf("brokers are required")
 	}
 
-	flag.Parse()
-
-	// Split the brokers' string into a slice
-	brokers := strings.Split(*kafkaBrokers, ",")
-	// Initialize a Kafka writer with the desired configuration
-	w := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: brokers,
-		Topic:   *kafkaTopic,                // Replace with your Kafka topic
+	kafkaBrokers := strings.Split(*brokers, ",")
+	writer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: kafkaBrokers,
+		Topic:   *topic,
 	})
-	defer func() {
-		if err := w.Close(); err != nil {
-			log.Println("Failed to close Kafka writer:", err)
-		}
-	}()
+	defer writer.Close()
 
 	var report types.Report
 	if err := json.NewDecoder(os.Stdin).Decode(&report); err != nil {
-		return fmt.Errorf("failed to decode json: %w", err)
+		return fmt.Errorf("failed to decode report: %w", err)
 	}
 
 	reportBytes, err := json.Marshal(report)
 	if err != nil {
-		return fmt.Errorf("failed to marshal report to json: %w", err)
+		return fmt.Errorf("failed to marshal report: %w", err)
 	}
 
-	msg := kafka.Message{
-		Key:   []byte("trivy"),
-		Value: reportBytes,
+	message := kafka.Message{Value: reportBytes}
+	if err := writer.WriteMessages(context.Background(), message); err != nil {
+		return fmt.Errorf("failed to send report to Kafka: %w", err)
 	}
 
-	// Write the message to Kafka
-	if err := w.WriteMessages(context.Background(), msg); err != nil {
-		return fmt.Errorf("failed to write to Kafka: %w", err)
-	}
-
-	// Optionally, you could print a confirmation message or handle logging
-	log.Println("Log has been sent to Kafka")
+	 severityCounts, totalCounts := countSeverities(report.Results)
+	 displaySeverityCounts(severityCounts, totalCounts)
 
 	return nil
+}
+
+func countSeverities(results []types.Result) (map[string]map[string]int, map[string]int) {
+	severityCounts := map[string]map[string]int{}
+	totalCounts := map[string]int{}
+
+	for _, result := range results {
+		severityCount, ok := severityCounts[result.Target]
+		if !ok {
+			severityCount = map[string]int{}
+			severityCounts[result.Target] = severityCount
+		}
+
+		for _, vuln := range result.Vulnerabilities {
+			severityCount[vuln.Severity]++
+			totalCounts[vuln.Severity]++
+		}
+	}
+
+	return severityCounts, totalCounts
+}
+
+func displaySeverityCounts(severityCounts map[string]map[string]int, totalCounts map[string]int) {
+	header := []string{"TARGET"}
+	uniqueSeverities := getUniqueSeverities(severityCounts)
+	header = append(header, uniqueSeverities...)
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader(header)
+
+	 for target, counts := range severityCounts {
+		row := []string{target}
+		for _, severity := range uniqueSeverities {
+			row = append(row, fmt.Sprintf("%d", counts[severity]))
+		}
+		table.Append(row)
+	}
+
+	totalRow := []string{"TOTAL SEVERITY"}
+	for _, severity := range uniqueSeverities {
+		totalRow = append(totalRow, fmt.Sprintf("%d", totalCounts[severity]))
+	}
+	table.SetFooter(totalRow)
+	table.SetCaption(true, fmt.Sprintf("Total severity: %d", sum(totalCounts)))
+	table.Render()
+}
+
+func sum(counts map[string]int) int {
+	total := 0
+	for _, count := range counts {
+		total += count
+	}
+	return total
+}
+
+func getUniqueSeverities(severityCountByTarget map[string]map[string]int) []string {
+	uniqueSeverities := map[string]struct{}{}
+	for _, severityCounts := range severityCountByTarget {
+		for severity := range severityCounts {
+			uniqueSeverities[severity] = struct{}{}
+		}
+	}
+
+	severities := make([]string, 0, len(uniqueSeverities))
+	for severity := range uniqueSeverities {
+		severities = append(severities, severity)
+	}
+
+	return severities
 }
